167ce40dceac8401d2ae2b6c8d9ef8b0a2d31589
sgrdevops1247 <sgrdevops1247@gmail.com>
2025-07-24 18:29:27 +0530

    functions

--- backend.sh
+++ backend.sh
@@ -36,6 +36,12 @@ VALIDATE $? "Enabling nodejs:20 version"
 dnf install nodejs -y &>>$LOGFILE
 VALIDATE $? "Installing nodejs"
 
-useradd expense
-VALIDATE $? "Creating expense user"
+id expense
+if [ $? -ne 0 ]
+then
+     useradd expense
+     VALIDATE $? "Creating expense user"
+else
+    echo "Expense user already created...$Y SKIPPING $N"
+fi
 
